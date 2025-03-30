3fe7dfd29972cf2b090cb45c52a8fac7fcf89e5d
Von <vonzedric012@gmail.com>
2025-03-30 17:14:58 +0800

    right_just.py

---
+++ batch_2/right_just.py
@@ -0,0 +1,11 @@
+def right_just(word, line_len):
+    space_len = line_len - len(word)
+    if space_len > 0:
+        return (' ' * space_len) + word
+    return word
+
+if __name__ == "__main__":
+    word_in = input("Input a string: ")
+    line_len = input("How long does the line should be? ")
+    word_out = right_just(word_in, line_len)
+    print("Output:", word_out)
